83d3494a588ed95fe2ffb715afc09f3333020c87
dsaf2007 <dsaf2007@naver.com>
2022-06-20 17:17:22 +0900

    project2 grover with 3 qubits

---
+++ project2/grover_3qubit.py
@@ -0,0 +1,69 @@
+#initialization
+import matplotlib.pyplot as plt
+import numpy as np
+
+# importing Qiskit
+from qiskit import IBMQ, Aer, assemble, transpile
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit.providers.ibmq import least_busy
+
+# import basic plot tools
+from qiskit.visualization import plot_histogram
+
+from qiskit_textbook.tools import vector2latex
+
+def initialize_s(qc, qubits):
+    """Apply a H-gate to 'qubits' in qc"""
+    for q in qubits:
+        qc.h(q)
+    return qc
+
+
+#Oracle that mark states |101> and |110> as the results
+qc = QuantumCircuit(3)
+qc.cz(0, 2)
+qc.cz(1, 2)
+oracle_ex3 = qc.to_gate()
+oracle_ex3.name = "U$_\omega$"
+
+
+#general Diffuser for any number of qubits
+def diffuser(nqubits):
+    qc = QuantumCircuit(nqubits)
+    # Apply transformation |s> -> |00..0> (H-gates)
+    for qubit in range(nqubits):
+        qc.h(qubit)
+    # Apply transformation |00..0> -> |11..1> (X-gates)
+    for qubit in range(nqubits):
+        qc.x(qubit)
+    # Do multi-controlled-Z gate
+    qc.h(nqubits-1)
+    qc.mct(list(range(nqubits-1)), nqubits-1)  # multi-controlled-toffoli
+    qc.h(nqubits-1)
+    # Apply transformation |11..1> -> |00..0>
+    for qubit in range(nqubits):
+        qc.x(qubit)
+    # Apply transformation |00..0> -> |s>
+    for qubit in range(nqubits):
+        qc.h(qubit)
+    # We will return the diffuser as a gate
+    U_s = qc.to_gate()
+    U_s.name = "U$_s$"
+    return U_s
+
+
+n = 3
+grover_circuit = QuantumCircuit(n)
+grover_circuit = initialize_s(grover_circuit, [0,1,2])
+grover_circuit.append(oracle_ex3, [0,1,2])
+grover_circuit.append(diffuser(n), [0,1,2])
+grover_circuit.measure_all()
+grover_circuit.draw(output='mpl',filename='circuit_3qubit.png')
+
+
+aer_sim = Aer.get_backend('aer_simulator')
+transpiled_grover_circuit = transpile(grover_circuit, aer_sim)
+qobj = assemble(transpiled_grover_circuit)
+results = aer_sim.run(qobj).result()
+counts = results.get_counts()
+plot_histogram(counts,filename='histogram_3qubits.png')
